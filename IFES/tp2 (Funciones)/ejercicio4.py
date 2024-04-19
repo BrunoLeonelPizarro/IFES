a4061edc3ec4b108bca18d23f9dd3ff1e859d70f
BrunoLeonelPizarro <leopizarro2004@gmail.com>
2024-04-19 20:48:11 -0300

    Se cambio el nombre de las carpetas

---
+++ IFES/tp2 (Funciones)/ejercicio4.py
@@ -0,0 +1,23 @@
+import os
+os.system("cls")
+
+#ejercicio 4
+def calcPerimetro (a,b):
+    perimetro = 2*a + 2*b
+    return perimetro
+
+
+
+def calcArea (a,b):
+    area = a*b
+    return area
+
+
+valor_Base = int(input("Ingresar el valor de la base: "))
+valor_altura = int(input("Ingresar el valor de la altura: "))
+print("\nValores del perimetro y el area: ")
+areaCuadrado = calcArea(valor_Base, valor_altura)
+perimetroCuadrado = calcPerimetro(valor_Base, valor_altura)
+print("Area:", areaCuadrado)
+print("perimetro: ",perimetroCuadrado)
+
