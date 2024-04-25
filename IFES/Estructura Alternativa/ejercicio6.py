c429af71042523457bb3e0378260c999da7199aa
BrunoLeonelPizarro <leopizarro2004@gmail.com>
2024-04-25 20:47:40 -0300

    un ejercicio modificado

--- IFES/Estructura Alternativa/ejercicio6.py
+++ IFES/Estructura Alternativa/ejercicio6.py
@@ -5,18 +5,22 @@ nombre = input("Ingrese el nombre: ")
 apellido = input("Ingrese el apellido: ")
 cod_Categoria = input("Ingrese la categoria del trabajador: ")
 
-if cod_Categoria == "A":
-    categoria = "Vendedor"
-    sueldo = 750000
-    print("El trabajador", nombre, apellido, "de la categoria", categoria, "tiene un salario de:", sueldo)
-elif cod_Categoria == "B":
-    categoria = "Administrativo"
-    sueldo = 820000
-    print("El trabajador", nombre, apellido, "de la categoria", categoria, "tiene un salario de:", sueldo)
-elif cod_Categoria == "C":
-    categoria = "Cajero"
-    sueldo = 780000
-    print("El trabajador", nombre, apellido, "de la categoria", categoria, "tiene un salario de:", sueldo)
+diccionario_Categorias = {"A":"Vendedor", "B":"Administrativo", "C":"Cajero"}
+diccionario_Sueldo = {"A":750000, "B":820000, "C":780000}
 
+if cod_Categoria != "A" and cod_Categoria != "B" and cod_Categoria != "C":
+    print ("Ingrese una categoria valida")
 else:
-    print("Categoria Invalida")
+    if cod_Categoria == "A":
+        categoria_Empleado = diccionario_Categorias ["A"]
+        sueldo_Empleado = diccionario_Sueldo["A"]
+    
+    elif cod_Categoria == "B":
+        categoria_Empleado = diccionario_Categorias["B"]
+        sueldo_Empleado = diccionario_Sueldo["B"]
+    
+    elif cod_Categoria == "C":
+        categoria_Empleado = diccionario_Categorias["C"]
+        sueldo_Empleado = diccionario_Sueldo["C"]
+        
+    print("El empleado", nombre, apellido, "de categoria", cod_Categoria, "tabaja en el puesto de", categoria_Empleado, "y tiene un salario de", sueldo_Empleado)
